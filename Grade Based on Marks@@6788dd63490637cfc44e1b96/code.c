ed1b8867699a615d4025a97671adc8231d5caa80
Sameer0705 <sameer0705.becse24@chitkara.edu.in>
2025-01-23 11:54:09 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -3,6 +3,6 @@
 int main() {
     int a;
     scanf("%d",&a);
-    (a>=90)?printf("A"):(90>a&&a>=80)?printf("B"):(80>a>=70)?printf("C"):(70>a>=60)?printf("D"):printf("F");
+    (a>=90)?printf("A"):(90>a&&a>=80)?printf("B"):(80>a&&a>=70)?printf("C"):(70>a&&a>=60)?printf("D"):printf("F");
     return 0;
 }
